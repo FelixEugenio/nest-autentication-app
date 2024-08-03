1900fb99501e1dc4ba38fdf03f3dddfdd85b1cff
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-04 00:02:09 +0100

    feat: creating hashed password funtion

---
+++ src/users/models/User.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+
