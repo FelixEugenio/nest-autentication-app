df49cc1b870dc4b25c460a0426ef683a223c6d2f
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-05 17:14:52 +0100

    feat: implementing usercontroller features

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignUpDTO } from './dto/signUp.dto';
+import { User } from './models/User';
+import { SignInDTO } from './dto/signIn.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('signup')
+    @HttpCode(HttpStatus.OK)
+    public async signUp(signUpDTO:SignUpDTO):Promise<User>{
+        return this.usersService.signUp(signUpDTO);
+    }
+
+    @Post('signup')
+    @HttpCode(200)
+    public async signIn(@Body() signInDTO:SignInDTO):Promise<{name:string,jwtToken:string,email:string}>{
+        return this.usersService.signIn(signInDTO);
+    }
+
+}
