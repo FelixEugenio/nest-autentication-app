9b36e4c8a147a6d41d5dc943dda1c26fb3612033
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-05 16:39:08 +0100

    feat: create signIn service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { User } from './models/User';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { AuthService } from 'src/auth/auth.service';
 import { SignUpDTO } from './dto/signUp.dto';
+import { SignInDTO } from './dto/signIn.dto';
 @Injectable()
 export class UsersService {
 
@@ -20,5 +21,18 @@ export class UsersService {
        const user = new this.userModel(signUpDTO);
        return user.save();
     }
+
+    public async signIn(signInDTO:SignInDTO):Promise<{name:string,jwtToken:string,email:string}>{
+         const user = await this.userModel.findByEmail({email:signInDTO.email});
+    }
+
+    private async findByEmail(email:string):Promise<User>{
+        const user = await this.userModel.findOne({email});
+        if(!user){
+            throw new NotFoundException('User not found');
+        }
+        return user;
+    }       
+     
 
 }
