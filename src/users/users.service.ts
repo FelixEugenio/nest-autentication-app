0595c97a46fe594f734942fc998fa6903b7363eb
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-05 16:21:59 +0100

    feat: inject dependencies inside constructor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import * as bcrypt from 'bcrypt';
+import { User } from './models/User';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { AuthService } from 'src/auth/auth.service';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    //criando constructor para injetar o model
+    constructor(
+        @InjectModel('User') 
+        private readonly userModel: Model<User>,
+        private readonly authService: AuthService
+    ) {} 
+
+    
+
+}
