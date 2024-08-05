1a0ebda817a212225192223e8e352294bf41e901
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-05 17:07:17 +0100

    feat: create findAll method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,7 +23,13 @@ export class UsersService {
     }
 
     public async signIn(signInDTO:SignInDTO):Promise<{name:string,jwtToken:string,email:string}>{
-         const user = await this.userModel.findByEmail({email:signInDTO.email});
+         const user = await this.findByEmail(signInDTO.email);
+         const isMatch = await this.checkPassword(signInDTO.password,user);
+         if(!isMatch){
+             throw new NotFoundException('Invalid Credentials');
+         }
+         const jwtToken = await this.authService.createAccessToken(user._id as string);
+         return {name:user.name,jwtToken,email:user.email}
     }
 
     private async findByEmail(email:string):Promise<User>{
@@ -32,7 +38,19 @@ export class UsersService {
             throw new NotFoundException('User not found');
         }
         return user;
-    }       
+    }   
+    
+    private async checkPassword(password:string,user:User):Promise<boolean>{
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            throw new NotFoundException('Wrong password');
+        }
+        return isMatch;
+    }
+
+    public async findAll():Promise<User[]>{
+        return this.userModel.find();
+    }
      
 
 }
